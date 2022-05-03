bb0621968d37e2f4dc741b7d0a84f75be116448d
HugoDELCROIX <hugo.delcroix@hotmail.com>
2022-05-03 19:31:46 +0200

    daylight cycle

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-$(document).mousemove(function(e){
-    $("#net").css({left:e.pageX-1, top:e.pageY-1});
-    
+$(document).mousemove(function (e) {
+    $("#net").css({
+        left: e.pageX - 1,
+        top: e.pageY - 1
+    });
 });
+
+setInterval(function () {
+    $("body").toggleClass('night');
+}, 5000);
